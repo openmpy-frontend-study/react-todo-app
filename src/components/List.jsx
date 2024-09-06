4b8d85314d3262f93743513f65930038fc8ecc7a
openmpy <suhwan@kakao.com>
2024-09-06 11:25:42 +0900

    UI 구현하기

---
+++ src/components/List.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+const List = () => {
+  return (
+    <div className="List">
+      <h4>Todo List 💩</h4>
+      <input type="text" placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default List;
