23e4dc8dbd6d670d902e30944d7891e7f5136c66
openmpy <suhwan@kakao.com>
2024-09-06 12:12:23 +0900

    Update - 투두 수정하기

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date }) => {
+const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
   return (
     <div className="TodoItem">
-      <input readOnly type="checkbox" checked={isDone} />
+      <input onChange={onChangeCheckbox} type="checkbox" checked={isDone} />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
